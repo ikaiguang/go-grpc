289616a33882ec77417e5d17b0632dff5a254fae
ikaiguang <ikaiguang@163.com>
2019-03-08 03:52:20 +0000

    添加protobuf/wen_json.proto

---
+++ protobuf/ikaiguang_protobuf_script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# load your env
+# source /etc/bashrc
+# source /etc/profile
+# source ~/.bashrc
+# source ~/.profile
+source ~/.bash_profile
+
+protoc -I. -I$GOPATH/src --go_out=. ./*.proto
+# protoc -I. -I$GOPATH/src --go_out=plugins=grpc:. ./*.proto
